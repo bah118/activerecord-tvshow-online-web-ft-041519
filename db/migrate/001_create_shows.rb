b72de4f17bb3c8ef1db63a0c9bee1f635239a805
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-03 21:31:28 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,11 +1,11 @@
-class CreateShows < ActiveRecord::Migration[5.2]
+class CreateShows < ActiveRecord::Migration[4.2]
   def change
     create_table :shows do |s|
       s.string :name 
       s.string :network
       s.string :day
       s.integer :rating 
-      s.timestamps
+      # s.timestamps
     end
   end
 end

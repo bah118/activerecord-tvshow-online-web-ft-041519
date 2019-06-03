0edc966ef14ae760e82b799c0209b70fa416914d
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-03 22:03:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,15 @@
 class Show < ActiveRecord::Base 
+  
   def self.highest_rating
     self.maximum("rating")
   end
   
   def self.most_popular_show
-    # popular = highest_rating
     Show.where(rating: highest_rating)[0]
-    
-    
   end
   
+  def self.lowest_rating
+    self.minimum("rating")
+  end
   
 end
